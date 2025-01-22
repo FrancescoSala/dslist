6eccc0b979e965340cac131887d10a6b5a5bd03d
Francesco Salamone <fransalamone@gmail.com>
2025-01-22 17:16:21 -0300

    SQL query, projection and games by list

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslist.services;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,13 @@ public class GameService {
     private GameRepository gameRepository;
 
     @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result =  gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
@@ -24,9 +32,9 @@ public class GameService {
     }
 
     @Transactional(readOnly = true)
-    public GameDTO findById(Long id) {
-        Game result = gameRepository.findById(id).get();
-        GameDTO dto = new GameDTO(result);
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result =  gameRepository.searchByList(listId);
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
     }
 
