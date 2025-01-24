9d6c86929371d85278b40b5c06bd504913c01c1c
Francesco Salamone <fransalamone@gmail.com>
2025-01-24 11:03:41 -0300

    Move games on the list

---
+++ src/main/java/com/devsuperior/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dslist.dto;
+
+public class ReplacementDTO {
+
+    private Integer sourceIndex;
+    private Integer destIndex;
+
+    public Integer getSourceIndex() {
+        return sourceIndex;
+    }
+
+    public void setSourceIndex(Integer sourceIndex) {
+        this.sourceIndex = sourceIndex;
+    }
+
+    public Integer getDestIndex() {
+        return destIndex;
+    }
+
+    public void setDestIndex(Integer destIndex) {
+        this.destIndex = destIndex;
+    }
+}
